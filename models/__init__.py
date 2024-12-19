e64c66c071e04b49960ea6790496814e53b3e45d
Uítalo Souza <uitalo.urss@gmail.com>
2024-12-19 11:26:39 -0300

    feat: criação dos models do projeto

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,4 @@
-from .user_model import UserModel
-from .task_model import TaskModel
+from .eventos_model import Evento
+from .organizador_model import Organizador
+from .participante_model import Participante
+from .participante_evento_model import ParticipanteEvento
---
+++ models/participante_evento_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from pytz import timezone
+from datetime import datetime
+
+from core.configs import settings
+
+class ParticipanteEvento(settings.DBBaseModel):
+    __tablename__ = "participantes_eventos"
+
+    evento_id = Column(Integer, ForeignKey('eventos.id'), primary_key=True)
+    participante_id = Column(UUID(as_uuid=True), ForeignKey('participantes.id'), primary_key=True)
+    criado_em = Column(DateTime, default=datetime.now(timezone("America/Bahia")))
