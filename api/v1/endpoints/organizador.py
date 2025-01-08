dded087f8d2ba11d04a384a6ed5b9057daccbb1b
Uítalo Souza <uitalo.urss@gmail.com>
2025-01-08 08:44:55 -0300

    refactor: Atualização do recurso organizador.

--- api/v1/endpoints/organizador.py
+++ api/v1/endpoints/organizador.py
@@ -17,8 +17,8 @@ async def post(organizador: OrganizadorCreateSchema, db: AsyncSession = Depends(
 async def get(db: AsyncSession = Depends(get_session), usuario_logado: Organizador = Depends(get_current_user)):
     return await get_organizador(organizador_id=usuario_logado.id, db=db)
 
-@router.put("/", response_model=OrganizadorSchema, status_code=status.HTTP_202_ACCEPTED, responses={**auth_responses, **generate_not_found_response("Organizador")})
-async def put(organizador: OrganizadorUpdateSchema, db: AsyncSession = Depends(get_session), usuario_logado: Organizador = Depends(get_current_user)):
+@router.patch("/", response_model=OrganizadorSchema, status_code=status.HTTP_202_ACCEPTED, responses={**auth_responses, **generate_not_found_response("Organizador")})
+async def patch(organizador: OrganizadorUpdateSchema, db: AsyncSession = Depends(get_session), usuario_logado: Organizador = Depends(get_current_user)):
     return await update_organizador(organizador_id=usuario_logado.id, organizador=organizador, db=db)
 
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT, responses={**auth_responses, **generate_not_found_response("Organizador")})
--- services/organizador_service.py
+++ services/organizador_service.py
@@ -25,9 +25,7 @@ async def create_organizador(organizador: OrganizadorCreateSchema, db: AsyncSess
         
 async def get_organizador(organizador_id: UUID, db: AsyncSession):
     async with db as session:
-        query = select(Organizador).filter(Organizador.id == organizador_id)
-        result = await session.execute(query)
-        organizador = result.scalars().unique().one_or_none()
+        organizador = await pegar_organizador(organizador_id=organizador_id, db=session)
 
         if organizador is None:
             raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Organizador não encontrado")
@@ -36,21 +34,22 @@ async def get_organizador(organizador_id: UUID, db: AsyncSession):
     
 async def update_organizador(organizador_id: UUID, organizador: OrganizadorUpdateSchema, db: AsyncSession):
     async with db as session:
-        query = select(Organizador).filter(Organizador.id == organizador_id)
-        result = await session.execute(query)
-        update_organizador = result.scalars().unique().one_or_none()
+        update_organizador = await pegar_organizador(organizador_id=organizador_id, db=session)
 
         if update_organizador is None:
             raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Organizador não encontrado")
         
-        if organizador.nome:
+        """if organizador.nome:
             update_organizador.nome = organizador.nome
 
         if organizador.email:
             update_organizador.email = organizador.email
 
         if organizador.telefone:
-            update_organizador.telefone = organizador.telefone
+            update_organizador.telefone = organizador.telefone"""
+
+        for field, value in organizador.model_dump(exclude_unset=True).items():
+            setattr(update_organizador, field, value)
     
         await session.commit()
 
@@ -58,9 +57,7 @@ async def update_organizador(organizador_id: UUID, organizador: OrganizadorUpdat
     
 async def delete_organizador(organizador_id: UUID, db: AsyncSession):
     async with db as session:
-        query = select(Organizador).filter(Organizador.id == organizador_id)
-        result = await session.execute(query)
-        delete_organizador = result.scalars().unique().one_or_none()
+        delete_organizador = await pegar_organizador(organizador_id=organizador_id, db=session)
 
         if delete_organizador is None:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Organizador não encontrado")
@@ -86,3 +83,11 @@ async def login_organizador(login_data: LoginSchema, db: AsyncSession):
         organizador_id = str(organizador.id)
 
         return create_access_token(sub=organizador_id, data_type="Organizador")
+    
+
+async def pegar_organizador(organizador_id: UUID, db: AsyncSession):
+    query = select(Organizador).filter(Organizador.id == organizador_id)
+    result = await db.execute(query)
+    organizador = result.scalars().unique().one_or_none()
+
+    return organizador
